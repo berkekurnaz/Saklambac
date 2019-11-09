06e774b478209bf97947de2bd69d7f982cead141
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-11-10 00:11:50 +0300

    Create Saklambac Test Projects

---
+++ src/Saklambac/Saklambac.TestNetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Saklambac.TestNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Saklambac");
+            Console.ReadLine();
+        }
+    }
+}
